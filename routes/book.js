ddb43a081f56aa97e84b71a002d9332e03b4c74e
mlinares <m.linares93@gmail.com>
2024-03-20 22:00:33 -0400

    create book router

---
+++ routes/book.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+
+export { router }
