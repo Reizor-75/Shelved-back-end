681463cbfe12b7a637ef698f06ba015c1a13e93d
mlinares <m.linares93@gmail.com>
2024-03-21 22:30:46 -0400

    update user model

--- routes/books.js
+++ routes/books.js
@@ -5,13 +5,12 @@ import * as booksCtrl from '../controllers/books.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
+router.get('/', booksCtrl.index)
+router.get('/:bookId', booksCtrl.show)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.get('/', checkAuth, booksCtrl.index)
 router.post('/create', checkAuth, booksCtrl.create)
-router.get('/:bookId', checkAuth, booksCtrl.show)
 router.put('/:bookId/update', checkAuth, booksCtrl.update)
 
 export { router }
