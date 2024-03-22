e62399ef0a31fd1490de11a338e29dc874e2ae4e
mlinares <m.linares93@gmail.com>
2024-03-21 22:01:38 -0400

    update book Model, create review Schema

--- models/book.js
+++ models/book.js
@@ -2,6 +2,21 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  reviwer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  },
+  title: String,
+  content: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  }
+})
+
 const bookSchema = new Schema({
   title: { 
     type: String,
@@ -10,6 +25,8 @@ const bookSchema = new Schema({
   author: [ {type: String, required: true} ],
   firstPublished: Date,
   genre: [ {type: String} ],
+  coverPhoto: String,
+  reviews: [reviewSchema],
 },{
   timestamps: true,
 })
