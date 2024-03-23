1398680e4e2d75ed14a0ac26e08cf22a9c13e387
mlinares <m.linares93@gmail.com>
2024-03-22 22:20:49 -0400

    create addToFollowing route to profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -12,10 +12,11 @@ router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 router.get('/:userId', checkAuth, profilesCtrl.show)
 router.put('/:userId/update', checkAuth, profilesCtrl.update)
+router.put('/:userId/following', checkAuth, profilesCtrl.addToFollowing)
 router.put('/:bookId', checkAuth, profilesCtrl.deleteBookReadList)
 router.put('/readlist/:bookId', checkAuth, profilesCtrl.deleteBookReadList)
-router.put('/wishlist/:bookId/updateList', checkAuth, profilesCtrl.moveBook)
 router.put('/wishlist/:bookId', checkAuth, profilesCtrl.deleteBookWishList)
+router.put('/wishlist/:bookId/updateList', checkAuth, profilesCtrl.moveBook)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 
 export { router }
