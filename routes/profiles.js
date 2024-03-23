372d0b2df39fdff78bab96c4853c07582b48af01
mlinares <m.linares93@gmail.com>
2024-03-22 21:50:33 -0400

    create deletebookWishList route in profile

--- routes/profiles.js
+++ routes/profiles.js
@@ -13,6 +13,7 @@ router.get('/', checkAuth, profilesCtrl.index)
 router.get('/show', checkAuth, profilesCtrl.show)
 router.put('/update', checkAuth, profilesCtrl.update)
 router.put('/readlist/:bookId', checkAuth, profilesCtrl.deleteBookReadList)
+router.put('/wishlist/:bookId', checkAuth, profilesCtrl.deleteBookWishList)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 
 export { router }
